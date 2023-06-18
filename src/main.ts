58cff7619e6d2c83d7e0b3cdfb70945f91345baa
hidems <miura.eleven@gmail.com>
2023-06-18 15:25:19 +0200

    Add xstate dependancy and remove unnecessary files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { inspect } from '@xstate/inspect'
+
+inspect({
+  iframe: false, // open in new window
+})
 
 createApp(App).mount('#app')
