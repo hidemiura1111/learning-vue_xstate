3aca38a75dcd98629d2a4fe85cd4749b954ba291
hidems <miura.eleven@gmail.com>
2023-06-23 22:56:40 +0200

    useMachine

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,21 @@
   <h1>Restaurant</h1>
 </template>
 
-<script setup lang="ts">
-</script>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useMachine } from '@xstate/vue'
+import fetchMachine from './fetchMachine'
 
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const { state, send } = useMachine(fetchMachine, { devTools  : true })
+  },
+})
+</script>
 
 <style scoped>
   h1 {
-    color: red;
+    color: white;
   }
 </style>
