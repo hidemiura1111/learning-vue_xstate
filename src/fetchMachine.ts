3f5153baeddf0882686925fee01ed9e0732979e8
hidems <miura.eleven@gmail.com>
2023-08-07 21:31:40 +0200

    Add fetchMAchine

--- src/fetchMachine.ts
+++ src/fetchMachine.ts
@@ -1,10 +1,104 @@
-import { createMachine } from "xstate";
+import { createMachine, assign } from "xstate";
+import { fetchRestaurants } from "./data";
 
-const fetchMachine = createMachine({
+interface FetchContext {
+  restaurants?: Restaurant[];
+  error?: Error;
+}
+
+type InitialContext = FetchContext & {
+  restaurants: undefined;
+  error: undefined;
+};
+
+type ReadyContext = FetchContext & {
+  restaurants: Restaurant[];
+  error: undefined;
+};
+
+type LoadingContext = FetchContext & {
+  restaurants: Restaurant[];
+  error: undefined;
+};
+
+type SuccessContext = FetchContext & {
+  restaurants: Restaurant[];
+  error: undefined;
+};
+
+type FailureContext = FetchContext & {
+  restaurants: undefined;
+  error: Error
+};
+
+type InitialState = { value: 'initial'; context: InitialContext };
+type ReadyState = { value: 'ready'; context: ReadyContext };
+type LoadingState = { value: 'loading'; context: LoadingContext };
+type SuccessState = { value: 'success'; context: SuccessContext };
+type FailureState = { value: 'failure'; context: FailureContext };
+
+type FetchState =
+  | InitialState
+  | ReadyState
+  | LoadingState
+  | SuccessState
+  | FailureState;
+
+type FetchEvent = { type: 'Fetch' } | { type: 'Retry' };
+
+const fetchMachine = createMachine<FetchContext, FetchEvent, FetchState>({
   id: "fetch",
   initial: "initial",
+  context: {
+    restaurants: undefined,
+    error: undefined
+  },
   states: {
-    initial: {}
+    initial: {
+      on: {
+        FETCH: {
+          target: 'loading',
+        },
+      },
+    },
+    ready: {
+      on: {
+        FETCH: 'loading',
+      },
+    },
+    loading: {
+      entry: assign({
+        restaurants: (context, _event) => context.restaurants || [],
+        error: (_context, _event) => undefined
+      }),
+      invoke: {
+        id: "getRestaurants",
+        src: (_context, _event) => fetchRestaurants(),
+        onDone: {
+          target: "success",
+          actions: assign({
+            restaurants: (_context, event) => event.data
+          })
+        },
+        onError: {
+          target: "failure",
+          actions: assign({
+            restaurants: (_context, _event) => undefined,
+            error: (_context, event) => event.data
+          })
+        }
+      }
+    },
+    success: {
+      after: {
+        2500: 'ready',
+      },
+    },
+    failure: {
+      on: {
+        RETRY: 'loading',
+      },
+    },
   }
 });
 
