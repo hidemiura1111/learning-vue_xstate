198539bb34b6e157baae1954b86ae0ff9df157be
hidems <miura.eleven@gmail.com>
2023-08-08 20:37:02 +0200

    Display state machine

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <template>
   <h1>Restaurant</h1>
+  <button
+    @click="send('FETCH')"
+    :disabled="!(state.matches('ready') || state.matches('initial'))"
+  >
+    FETCH
+  </button>
+
+  <button
+    @click="send('RETRY')"
+    :disabled="!state.matches('failure')"
+  >
+    RETRY
+  </button>
+
+  <span v-show="state.matches('loading')">...Loading...</span>
+
+  <div
+    v-if="
+    state.matches('ready') ||
+    state.matches('success') ||
+    state.matches('loading')
+  ">
+    <div :key="restaurant.id" v-for="restaurant in state.context.restaurants">
+      <h3>{{ restaurant.name }}</h3>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,7 +36,12 @@ import fetchMachine from './fetchMachine'
 export default defineComponent({
   name: 'App',
   setup() {
-    const { state, send } = useMachine(fetchMachine, { devTools  : true })
+    const { state, send } = useMachine(fetchMachine, { devTools: true })
+
+    return {
+      state,
+      send,
+    }
   },
 })
 </script>
@@ -18,5 +49,8 @@ export default defineComponent({
 <style scoped>
   h1 {
     color: white;
+  }
+  button {
+    margin: 10px;
   }
 </style>
